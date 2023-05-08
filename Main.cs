5ba6b888a572e82d986b0cd91276cd33c93cb8f1
SvitlanaHalaiba <svitlana.halaiba@globallogic.com>
2023-05-08 12:29:59 +0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using CartSpase;
+static void Main(string[] args)
+{
+    // Create a new cart with the user ID "test"
+    Cart cart = new Cart("test");
+
+    // Add some items to the cart
+    cart.AddItem("item1", 2, 10.99m);
+    cart.AddItem("item2", 1, 5.99m);
+
+    // Print the total cost of the cart
+    Console.WriteLine("Cart total: " + cart.CartAmount.ToString("C"));
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace CartSpase
+{
+    public class Cart
+    {
+        private string userId;
+        private Dictionary<string, int> itemQuantities = new Dictionary<string, int>();
+        private Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>();
+
+        public Cart(string userId)
+        {
+            this.userId = userId;
+        }
+
+        public decimal CartAmount
+        {
+            get
+            {
+                decimal amount = 0;
+                foreach (var item in itemQuantities)
+                {
+                    amount += item.Value * itemPrices[item.Key];
+                }
+                return amount;
+            }
+        }
+
+        public int CartCount
+        {
+            get { return itemQuantities.Values.Sum(); }
+        }
+
+        public void AddItem(string itemId, int quantity, decimal price)
+        {
+            if (price <= 0)
+            {
+                throw new ArgumentException("Price must be positive", nameof(price));
+            }
+
+            if (itemQuantities.TryGetValue(itemId, out int currentQuantity))
+            {
+                if (itemPrices[itemId] != price)
+                {
+                    throw new ArgumentException("Cannot change the price of an item that is already in the cart", nameof(price));
+                }
+
+                itemQuantities[itemId] += quantity;
+            }
+            else
+            {
+                itemQuantities[itemId] = quantity;
+                itemPrices[itemId] = price;
+            }
+        }
+
+        public void RemoveItem(string itemId, int quantity)
+        {
+            if (itemQuantities.TryGetValue(itemId, out int currentQuantity))
+            {
+                if (currentQuantity < quantity)
+                {
+                    throw new ArgumentException("Cannot remove more items than are in the cart", nameof(quantity));
+                }
+
+                currentQuantity -= quantity;
+                if (currentQuantity == 0)
+                {
+                    itemQuantities.Remove(itemId);
+                    itemPrices.Remove(itemId);
+                }
+                else
+                {
+                    itemQuantities[itemId] = currentQuantity;
+                }
+            }
+        }
+    }
+}
